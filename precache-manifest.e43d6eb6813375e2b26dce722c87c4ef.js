93047f2e2c907a16854d50fdbb663ecface280b7
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-09-15 17:02:13 -0700

    Updates

---
+++ precache-manifest.e43d6eb6813375e2b26dce722c87c4ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410225733f6bcdc512a7a5578905e951",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "c37b75755d762cd01a37",
+    "url": "/react-todo-app/static/css/main.76113079.chunk.css"
+  },
+  {
+    "revision": "b045ded14fb40efb1157",
+    "url": "/react-todo-app/static/js/2.f278f063.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.f278f063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37b75755d762cd01a37",
+    "url": "/react-todo-app/static/js/main.00c6dbad.chunk.js"
+  },
+  {
+    "revision": "1528fb5b7daed31266f4",
+    "url": "/react-todo-app/static/js/runtime-main.1dd0cb46.js"
+  }
+]);
